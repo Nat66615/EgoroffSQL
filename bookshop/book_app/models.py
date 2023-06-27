74d7f09418882e8caf215aed638957482d280404
Nat66615 <nat.ja@mail.ru>
2023-06-27 15:05:01 +0500

    4.7 complette

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return f'{self.title} - {self.rating}%'
+        return f'{self.title} - {self.author} - {self.rating}% - {self.is_best_selling}'
