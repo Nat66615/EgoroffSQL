89391670c9e106bb340b89489873b33d4c61df19
Nat66615 <nat.ja@mail.ru>
2023-06-30 15:38:40 +0500

    4.12 slug

--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -5,12 +5,14 @@ from .models import Movie
 # Create your views here.
 def show_all_movie(request):
     movies = Movie.objects.all()
+    #for movie in movies: - проходим по названиям фильмов и получаем для каждого слаг название
+    #   movie.save()
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies
     })
 
-def show_one_movie(request, id_movie:int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie:str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie': movie
     })
